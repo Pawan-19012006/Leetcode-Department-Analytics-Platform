8a62a488ceac6a998112b43676476902222d94bf
Pawan Eswaran <pawaneswaran19012006@gmail.com>
2026-06-20 01:35:23 +0530

    feat: created contest repository and contest result repository

---
+++ app/repositories/contest_repository.py
@@ -0,0 +1,69 @@
+from app.models.contest import Contest
+
+
+def get_by_name(
+    db,
+    contest_name
+):
+
+    return (
+        db.query(Contest)
+        .filter(
+            Contest.contest_name == contest_name
+        )
+        .first()
+    )
+
+
+def create_contest(
+    db,
+    contest_name,
+    contest_type,
+    contest_number,
+    contest_date
+):
+
+    contest = Contest(
+
+        contest_name=contest_name,
+
+        contest_type=contest_type,
+
+        contest_number=contest_number,
+
+        contest_date=contest_date
+
+    )
+
+    db.add(contest)
+
+    db.commit()
+
+    db.refresh(contest)
+
+    return contest
+
+
+def get_or_create_contest(
+    db,
+    contest_name,
+    contest_type,
+    contest_number,
+    contest_date
+):
+
+    contest = get_by_name(
+        db,
+        contest_name
+    )
+
+    if contest:
+        return contest
+
+    return create_contest(
+        db,
+        contest_name,
+        contest_type,
+        contest_number,
+        contest_date
+    )
---
+++ app/repositories/contest_result_repository.py
@@ -0,0 +1,65 @@
+from app.models.contest_result import ContestResult
+
+
+def create_result(
+    db,
+    student_id,
+    contest_id,
+    result_data
+):
+
+    result = ContestResult(
+
+        student_id=student_id,
+
+        contest_id=contest_id,
+
+        global_rank=result_data["ranking"],
+
+        problems_solved=result_data["problemsSolved"],
+
+        total_problems=result_data["totalProblems"],
+
+        finish_time_seconds=result_data["finishTimeInSeconds"],
+
+        rating_after=result_data["rating"],
+
+        rating_change=result_data["rating_change"]
+
+    )
+
+    db.add(result)
+
+    db.commit()
+
+    db.refresh(result)
+
+    return result
+
+
+def get_results_by_student(
+    db,
+    student_id
+):
+
+    return (
+        db.query(ContestResult)
+        .filter(
+            ContestResult.student_id == student_id
+        )
+        .all()
+    )
+
+
+def get_results_by_contest(
+    db,
+    contest_id
+):
+
+    return (
+        db.query(ContestResult)
+        .filter(
+            ContestResult.contest_id == contest_id
+        )
+        .all()
+    )
